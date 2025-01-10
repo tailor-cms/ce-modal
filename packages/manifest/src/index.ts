1bc145db47253bcdefff8e9b7eaf79b5678b7b2e
rkusan00 <renato.kusanovic@gmail.com>
2025-01-10 17:27:07 +0100

    Update icon

--- packages/manifest/src/index.ts
+++ packages/manifest/src/index.ts
@@ -23,7 +23,7 @@ export const version = '1.0';
 // UI configuration for Tailor CMS
 const ui = {
   // Display icon, https://pictogrammers.com/library/mdi/
-  icon: 'mdi-window-maximize',
+  icon: 'mdi-window-restore',
   // Does element support only full width or can be used within layouts
   // (e.g. 50/50 layout)
   forceFullWidth: false,
