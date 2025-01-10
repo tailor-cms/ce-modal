562994ca1a5914ce26ae93b63d6a885a8379f290
rkusan00 <renato.kusanovic@gmail.com>
2025-01-10 17:26:23 +0100

    Implement authoring component

--- packages/edit/src/components/TopToolbar.vue
+++ packages/edit/src/components/TopToolbar.vue
@@ -1,14 +1,37 @@
 <template>
-  <div>
-    <p>Edit element top toolbar {{ element?.id }}</p>
-  </div>
+  <VForm ref="form" validate-on="submit">
+    <VToolbarItems class="ga-2">
+      <VTextField
+        v-model="title"
+        class="required ml-2"
+        hide-details="auto"
+        label="Button label"
+        min-width="200"
+        prepend-inner-icon="mdi-gesture-tap-button"
+        variant="outlined"
+      />
+    </VToolbarItems>
+  </VForm>
 </template>
 
 <script setup lang="ts">
-import { defineEmits, defineProps } from 'vue';
+import { inject, ref, watch } from 'vue';
+import debounce from 'lodash/debounce';
+import type { Element } from '@tailor-cms/ce-modal-manifest';
 
-defineProps<{ element: Element }>();
-defineEmits(['save']);
+const props = defineProps<{ element: Element }>();
+
+const elementBus: any = inject('$elementBus');
+
+const form = ref<HTMLFormElement>();
+const title = ref(props.element.data.title);
+
+watch(
+  title,
+  debounce(async () => {
+    if (!form.value) return;
+    const { valid } = await form.value.validate();
+    if (valid) return elementBus.emit('title', title.value);
+  }, 500),
+);
 </script>
-
-<style scoped></style>
